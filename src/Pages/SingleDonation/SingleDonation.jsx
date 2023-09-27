a7c735136fd540f136ef9c4926fe49bcd306877a
Saifulkhandaker <khsaiful67@gmail.com>
2023-09-27 12:32:03 +0600

    image and button positon error fixed in donation route

--- src/Pages/SingleDonation/SingleDonation.jsx
+++ src/Pages/SingleDonation/SingleDonation.jsx
@@ -44,15 +44,15 @@ const SingleDonation = () => {
 
   return (
      <div className="h-screen">
-        <div className="">
-            <img className="relative h-[80vh] md:ml-28 w-3/4 mx-auto rounded-lg" src={picture} alt="" />
-            <div className="absolute h-fit hero-overlay bg-opacity-60 bottom-[-51px] left-[68px] w-[290px] md:left-36 md:-bottom-2 md:w-[528px] lg:bottom-[15px] lg:left-[170px] lg:w-[925px] py-2 md:py-4 px-4">
+        <div className="relative">
+            <img className="w-full h-[90vh] rounded-lg" src={picture} alt="" />
+            <div className="absolute h-fit hero-overlay bg-opacity-60 bottom-0 left-0 w-full  py-2 md:py-4 px-4">
             <button 
             onClick={handleDonate}
             className="btn border-none rounded-sm text-white" style={{background: text_button_bg_color}} >Donate {price}</button>
             </div>
         </div>
-        <div className="w-5/6 mx-auto mt-10">
+        <div className="w-full  mt-10">
           <h1 className="text-3xl font-bold">{title}</h1>
           <p className="mt-2 space-y-2">{description}</p>
         </div>
