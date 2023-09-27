1fcf1ac257fe864f8a5c9aac642794f5a9edce33
Saifulkhandaker <khsaiful67@gmail.com>
2023-09-27 15:01:14 +0600

    name removed when showing parsentege in pie chart

--- src/Pages/Statistics/Statistics.jsx
+++ src/Pages/Statistics/Statistics.jsx
@@ -42,7 +42,7 @@ const Statistics = () => {
               outerRadius={80}
               fill="#8884d8"
               dataKey="value"
-              label={({ name, percent }) => `${name}: ${(percent * 100).toFixed(0)}%`}
+              label={({percent }) => `${(percent * 100).toFixed(0)}%`}
             >
               {data.map((entry, index) => (
                 <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
